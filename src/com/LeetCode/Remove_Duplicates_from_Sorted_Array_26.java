440f87df9d24d84444862e39faffd50d34a80fd6
toys416 <toys416@gmail.com>
2017-05-16 17:37:57 +0800

    update

--- src/com/LeetCode/Remove_Duplicates_from_Sorted_Array_26.java
+++ src/com/LeetCode/Remove_Duplicates_from_Sorted_Array_26.java
@@ -3,7 +3,7 @@ package com.LeetCode;
 public class Remove_Duplicates_from_Sorted_Array_26 {
 	public static int removeDuplicates(int[] nums) {
 		int i=0;
-		for (int j=0;j<nums.length;j++){
+		for (int j=1;j<nums.length;j++){
 			if(nums[j]!=nums[j+1]){
 				nums[i]=nums[j];
 				i++;
@@ -14,10 +14,29 @@ public class Remove_Duplicates_from_Sorted_Array_26 {
 
 	}
 	
+/*
+ Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while jj is the fast-runner. 
+ As long as nums[i] = nums[j]nums[i]=nums[j], we increment jj to skip the duplicate.
+
+When we encounter nums[j] \neq nums[i]nums[j]≠nums[i], the duplicate run has ended so we must copy its value to nums[i + 1]nums[i+1]. 
+i is then incremented and we repeat the same process again until jj reaches the end of array.
+ */
+	public static int removeDuplicates_good(int[] nums) {
+	    if (nums.length == 0) return 0;
+	    int i = 0;
+	    for (int j = 1; j < nums.length; j++) {
+	        if (nums[j] != nums[i]) {
+	            i++;
+	            nums[i] = nums[j];
+	        }
+	    }
+	    return i + 1;
+	}
+	
 	
 	public static void main(String args[]) {
 		int a[] = { 1,1,2 };
-		int b = removeDuplicates(a);
+		int b = removeDuplicates_good(a);
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i]);
 		}
